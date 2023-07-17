d6ee61410f2b35de22dc4357ae56574337c778d4
Igor Gonçalves <igordossantos38@gmail.com>
2023-07-17 12:01:50 -0300

    feat: creating an express server

---
+++ src/config/server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const port = 3000
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, () => {
+    console.log(`Server running at por ${port}`)
+})
